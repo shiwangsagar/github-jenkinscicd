3a2e3065f585c1e5db965029e666344db836cf8a
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-01-31 23:18:15 +0000

    refine

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-docker run --name jenkins -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) brainupgrade/jenkins:docker20221110
+# Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# Create a Kubernetes cluster
+kind create cluster --name k8s --config cluster.yaml
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#docker run --name jenkins -d -p 8080:8080  -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker)  brainupgrade/jenkins:2.414.1x3
+kubectl apply -f https://raw.githubusercontent.com/brainupgrade-in/dockerk8s/main/kubernetes/lab/07-statefulset/jenkins/jenkins.yaml
