1c5f4817c3b6f72539faa9c5dd6002754b041c70
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-01-31 23:34:31 +0000

    refine

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -11,5 +11,5 @@ kind create cluster --name k8s --config cluster.yaml
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-#docker run --name jenkins -d -p 8080:8080  -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker)  brainupgrade/jenkins:2.414.1x3
+docker run --name jenkins -d -p 8080:8080  -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker)  brainupgrade/jenkins:2.414.1x3
 kubectl apply -f https://raw.githubusercontent.com/brainupgrade-in/dockerk8s/main/kubernetes/lab/07-statefulset/jenkins/jenkins.yaml
