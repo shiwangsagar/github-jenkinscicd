e39df8d4558514b62c44e256811745ad5d4e7a59
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-01-31 23:01:17 +0000

    refine

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name jenkins -d -p 8080:8080 brainupgrade/jenkins:2.414.1x3
+docker run --name jenkins -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) brainupgrade/jenkins:docker20221110
